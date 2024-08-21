b6b726cdf38534941fddb42c42981e27f3b13e3c
davipythonweb <galaxys3tab32@gmail.com>
2024-08-21 03:46:57 -0300

    update comentario

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 	"net/http"
 )
 
+// variavel que carrega o template
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
+// funcao para receber a request da rota  e criar  o server
 func main() {
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":9000", nil)
 }
 
+// função para renderizar o template apontando para o http.Request
 func index(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "index", nil)
 }
