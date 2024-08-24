675613be7ec560600e341ed092e77dbaf459d681
davipythonweb <galaxys3tab32@gmail.com>
2024-08-24 20:53:58 -0300

    update project

--- sql.sql
+++ sql.sql
@@ -7,13 +7,13 @@ create table produtos (
 	preco decimal,
 	quantidade integer
 )
-,
+
 -- - codigo sql para inserir um produto no banco:
 
 insert into produtos (nome, descricao, preco, quantidade) values
 ('camisa', 'vinho', 23.50, 10),
 ('Bone', 'preto', 65.75, 3);
-,
+
 
 -- - fazendo um filtro sql para mostrar as 100 primeiras linhas da tabela:
 
